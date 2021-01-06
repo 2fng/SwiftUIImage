b01fc35330c6304cc6ed7b541bc78fb5e4e072b7
Hua Son Tung <tunghuason@gmail.com>
2021-01-07 02:14:47 +0700

    Initial Commit

---
+++ SwiftUIImage/SwiftUIImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIImageApp.swift
+//  SwiftUIImage
+//
+//  Created by Hua Son Tung on 07/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
