0e76c82ffebbdad9327ca97b50ad20f3f38900ee
Hua Son Tung <tunghuason@gmail.com>
2021-01-09 00:15:37 +0700

    Version 1.1

--- SwiftUIImage/ContentView.swift
+++ SwiftUIImage/ContentView.swift
@@ -9,8 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Image("paris")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 350)
+            .overlay(
+                Color.black
+                    .opacity(0.4)
+                    .overlay(
+                        Text("Paris")
+                            .font(.largeTitle)
+                            .fontWeight(.black)
+                            .foregroundColor(.white)
+                            .frame(width: 200)
+        ) )
     }
 }
 
